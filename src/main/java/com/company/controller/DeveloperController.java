b57c8b11dc1121e4c266bca90b7a2fa4bb9e3963
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-13 08:14:51 +0530

    Added API Controller to handle file upload

--- src/main/java/com/company/controller/DeveloperController.java
+++ src/main/java/com/company/controller/DeveloperController.java
@@ -2,16 +2,15 @@ package com.company.controller;
 
 import com.company.entity.Developer;
 import com.company.service.DeveloperService;
-import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/developer")
+
 public class DeveloperController {
 
     @Autowired
