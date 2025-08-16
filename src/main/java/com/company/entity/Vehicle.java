2fa3cd7e188fd171553422e0c6504c8457b56773
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-16 22:37:31 +0530

    Added Vehicle class

---
+++ src/main/java/com/company/entity/Vehicle.java
@@ -0,0 +1,53 @@
+package com.company.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vehicleId;
+
+    private String vehicleType;
+    private String registrationNumber;
+
+    // Many Vehicles -> One Developer
+    @ManyToOne
+    @JoinColumn(name = "Developer_id")  // foreign key in Vehicle table
+    private Developer Developer;
+
+    // Getters & Setters
+    public Long getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public Developer getDeveloper() {
+        return Developer;
+    }
+
+    public void setDeveloper(Developer Developer) {
+        this.Developer = Developer;
+    }
+}
+
