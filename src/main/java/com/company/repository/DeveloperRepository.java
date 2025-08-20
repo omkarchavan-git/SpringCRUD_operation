2f810536b9044757579277787ccefb7f55315da8
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-08-21 00:02:34 +0530

    Implemented MySQL method

--- src/main/java/com/company/repository/DeveloperRepository.java
+++ src/main/java/com/company/repository/DeveloperRepository.java
@@ -3,6 +3,7 @@ package com.company.repository;
 import com.company.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,8 +19,8 @@ public interface DeveloperRepository extends JpaRepository<Developer, Integer> {
    List<Developer> findByCity(String city);
 
     // adding MySQL query to get data by age
-    @Query(value = "SELECT * FROM developer WHERE age =:age", nativeQuery = true)
-    List<Developer> findByAge(int age);
+    @Query(value = "SELECT * FROM developer WHERE age = :age", nativeQuery = true)
+    List<Developer> findByAge(@Param("age") int age);
 
 
 
